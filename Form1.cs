e88036a019784347cbf320642ea1f715a5d0f59c
JDi-Coding <148185445+JDi-Coding@users.noreply.github.com>
2025-02-14 18:41:37 +0100

    Succesful output of Filenames

--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,19 @@ namespace DownloadCleanerV2
         {
             InitializeComponent();
         }
+
+        public static string[] GetFiles(string path)
+        {
+            return System.IO.Directory.GetFiles(path);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string[] Files = GetFiles(@"E:\Downloads\");
+            foreach (string File in Files)
+            {
+                textBox1.Text += File + Environment.NewLine;
+            }
+        }
     }
 }
